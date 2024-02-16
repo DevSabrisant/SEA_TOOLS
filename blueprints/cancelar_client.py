d716d18b8948174e355f754b95dcf16aa998754f
CAIO <caiodevanalista@gmail.com>
2024-02-16 02:02:33 -0300

    Adição Cancelamento
    Calculo de cancelamento
    
    Proporcional de consumo + Costas de multa.

--- blueprints/cancelar_client.py
+++ blueprints/cancelar_client.py
@@ -1,39 +1,122 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from blueprints.planos import *
 import calendar
+from dateutil.relativedelta import relativedelta
 
-def Calculo_cancelamento(pAtual, pVen, Data_Solicitacao,data_ati):
+def Calculo_cancelamento(pAtual, pVen, Data_Solicitacao, data_ati, multa):
 
     data_hoje = datetime.strptime(Data_Solicitacao, "%Y-%m-%d").date() if (Data_Solicitacao != "") and (Data_Solicitacao != None) else date.today()
     Data_SolicitacaoBr = data_hoje.strftime("%d-%m-%Y")
-
-    #Ant_IniVenc = date(data_hoje.year - 1 if data_hoje.month == 1 else data_hoje.year, data_hoje.month - 1 if data_hoje.month != 1 else 12, 1)
-    #Ant_IniVencBr = Ant_IniVenc.strftime("%d/%m/%Y")
-    #Atu_FinalVenc = date(data_hoje.year + 1 if ProxAtual == 1 else data_hoje.year, data_hoje.month, quantidade_dias)
-    #Atu_FinalVencBr = Atu_FinalVenc.strftime("%d/%m/%Y")
-
-    #IniVenc = date(data_hoje.year, data_hoje.month, 1)
-    #IniVencBr = IniVenc01.strftime("%d/%m/%Y")
-    #FinalVenc = date(data_hoje.year + 1 if ProxAtual == 1 else data_hoje.year, ProxAtual, Prox_quantidade_dias)
-    #FinalVencBr = FinalVenc.strftime("%d/%m/%Y")
 
     Ant_quantidade_dias = calendar.monthrange(data_hoje.year - 1 if data_hoje.month == 1 else data_hoje.year, data_hoje.month - 1 if data_hoje.month != 1 else 12)[1]
 
     quantidade_dias = calendar.monthrange(data_hoje.year, data_hoje.month)[1]
 
+    ProxAtual = (data_hoje.month % 12) + 1
+
     DadosVen = [
+
         (11, 10) if pVen == "15"
         else (11, 10) if pVen == "20"
         else (21, 20) if pVen == "25"
         else (21, 20) if pVen == "30"
-        else None]
+        else (1, quantidade_dias) if pVen == "5"
+        else (1, quantidade_dias) if pVen == "10"
+        else (0, 0)
+    ]
 
+    if data_ati is not None:
 
-    if data_hoje <= DadosVen[1]:
-        r = f""
-        return r
+        Ant_IniVenc = date(data_hoje.year - 1 if data_hoje.month == 1 else data_hoje.year,
+                           data_hoje.month - 1 if data_hoje.month != 1 else 12, DadosVen[0][0])
+        Ant_IniVencBr = Ant_IniVenc.strftime("%d/%m/%Y")
+        Atu_FinalVenc = date(data_hoje.year + 1 if ProxAtual == 1 else data_hoje.year, data_hoje.month, DadosVen[0][1])
+        Atu_FinalVencBr = Atu_FinalVenc.strftime("%d/%m/%Y")
+
+        IniVenc = date(data_hoje.year, data_hoje.month, DadosVen[0][0])
+        IniVencBr = IniVenc.strftime("%d/%m/%Y")
+        FinalVenc = date(data_hoje.year + 1 if ProxAtual == 1 else data_hoje.year, ProxAtual, DadosVen[0][1])
+        FinalVencBr = FinalVenc.strftime("%d/%m/%Y")
+
+        HojeBr = data_hoje.strftime("%d/%m/%Y")
+
+        ativa_cliente = datetime.strptime(data_ati, "%Y-%m-%d").date()
+        ativa_clienteBr = ativa_cliente.strftime("%d/%m/%Y")
+
+        venc_fidelidade = ativa_cliente + relativedelta(months=12)
+
+        Multa_Cliente = int((venc_fidelidade - data_hoje).days / 30) * 60
+
+        if (DadosVen[0][0] == 0) or (DadosVen[0][1] == 0):
+            r = "Resultado"
+            return r
+        elif (pVen == "5") or (pVen == "10"):
+            if multa:
+                data1 = IniVenc
+                data2 = data_hoje
+                Qtd = data2 - data1
+
+                Valor = Qtd.days * (globals()[f"plan{pAtual}"]() / 30)
+
+                r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\nMulta: Sim\n\n{IniVencBr} - {HojeBr} são - {Qtd.days} dias - Total: {Valor:.2f}\n" \
+                    f"Ativação: {ativa_clienteBr}\nMulta: {Multa_Cliente}"
+                return r
+
+            else:
+                data1 = IniVenc
+                data2 = data_hoje
+                Qtd = data2 - data1
+
+                Valor = Qtd.days * (globals()[f"plan{pAtual}"]() / 30)
+
+                r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\nMulta: Não\n\n{IniVencBr} - {HojeBr} são - {Qtd.days} dias - Total: {Valor:.2f}\n"
+                return r
+
+        else:
+            if multa:
+                if data_hoje.day <= DadosVen[0][0]:
+
+                    data1 = Ant_IniVenc
+                    data2 = data_hoje
+                    Qtd = data2 - data1
+
+                    Valor = Qtd.days * (globals()[f"plan{pAtual}"]() / 30)
+
+                    r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\nMulta: Sim\n\n{Ant_IniVencBr} - {HojeBr} são - {Qtd.days} dias - Total: {Valor:.2f}\n" \
+                        f"Ativação: {ativa_clienteBr}\nMulta: {Multa_Cliente}"
+                    return r
+                else:
+                    data1 = IniVenc
+                    data2 = data_hoje
+                    Qtd = data2 - data1
+
+                    Valor = Qtd.days * (globals()[f"plan{pAtual}"]() / 30)
+
+                    r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\nMulta: Sim\n\n{IniVencBr} - {HojeBr} são - {Qtd.days} dias - Total: {Valor:.2f}\n" \
+                        f"Ativação: {ativa_clienteBr}\nMulta: {Multa_Cliente}"
+                    return r
+            else:
+                if data_hoje.day <= DadosVen[0][0]:
+                    data1 = Ant_IniVenc
+                    data2 = data_hoje
+                    Qtd = data2 - data1
+
+                    Valor = Qtd.days * (globals()[f"plan{pAtual}"]() / 30)
+
+                    r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\nMulta: Sim\n\n{Ant_IniVencBr} - {HojeBr} são - {Qtd.days} dias - Total: {Valor:.2f}\n"
+                    return r
+                else:
+                    data1 = IniVenc
+                    data2 = data_hoje
+                    Qtd = data2 - data1
+
+                    Valor = Qtd.days * (globals()[f"plan{pAtual}"]() / 30)
+
+                    r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\nMulta: Sim\n\n{IniVencBr} - {HojeBr} são - {Qtd.days} dias - Total: {Valor:.2f}\n"
+                    return r
+
+            return r
     else:
-        r = f""
-        return r
+        r = "Resultado"
 
-    r = f"Data Simulada: {Data_SolicitacaoBr}\nVencimento: {pVen}\nPlano: {pAtual}\n\n"
-    return r
+        return r
