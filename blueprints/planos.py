05a1c3fdbbf881db4d49ee2b8e9d134f58d38777
CAIO <119272627+CaioLir4@users.noreply.github.com>
2024-03-26 12:12:58 -0300

    Update planos.py
    Novos valores

--- blueprints/planos.py
+++ blueprints/planos.py
@@ -1,32 +1,32 @@
 def plan500_Connect():
-    i = 110
+    i = 99
     return i
 def plan250():
-    i = 110
+    i = 99
     return i
 def plan600_Super():
-    i = 130
+    i = 117
     return i
 def plan350():
-    i = 130
+    i = 117
     return i
 def plan700_Hiper():
-    i = 160
+    i = 144
     return i
 def plan450():
-    i = 160
+    i = 144
     return i
 def plan800_Ultra():
-    i = 210
+    i = 189
     return i
 def plan600():
-    i = 210
+    i = 189
     return i
 def plan500up():
-    i = 144.33
+    i = 129.90
     return i
 def plan700up():
-    i = 177.67
+    i = 159.90
     return  i
 def planNone():
     return 0
