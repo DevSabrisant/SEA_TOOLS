fe25142c42a4b627400cfed6ea6b7b358f37e409
CaioLir4 <caiodevanalista@gmail.com>
2024-04-17 00:24:43 -0300

    up

--- database/database.py
+++ database/database.py
@@ -3,10 +3,9 @@ from dotenv import load_dotenv
 from peewee import SqliteDatabase
 import os
 
-
 load_dotenv()
 
-#db = PostgresqlDatabase(os.getenv('DATABASE_URI', ''))
+db = PostgresqlDatabase(os.getenv('DATABASE_URI', ''))
 
 
-db = SqliteDatabase('dbseatools.db')
+#db = SqliteDatabase('dbseatools.db')
