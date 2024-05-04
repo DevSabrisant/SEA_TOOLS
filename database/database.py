728f1ab2cdd9bdfdb501d03f895a9fd2fb94e809
CaioLir4 <caiodevanalista@gmail.com>
2024-05-04 14:09:57 -0300

    Informações sobre o BD

--- database/database.py
+++ database/database.py
@@ -5,7 +5,17 @@ import os
 
 load_dotenv()
 
+#Descomente o #  e utilize a versão que voce desejar.
+
+#Versão para servidor remoto, hospedagem remota. Dados de acesso ficam no servidor remoto.
 db = PostgresqlDatabase(os.getenv('DATABASE_URI', ''))
+
+
+
+#Versão para banco de dados local, usar com Docker. Colocar dentro da string a url de acesso ao banco.
 #db = PostgresqlDatabase('')
 
+
+
+#Versão sqlite para desenvolvimento. É criado o banco autimaticamente.
 #db = SqliteDatabase('dbseatools.db')
